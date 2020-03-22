b9e9c7edb0f14e54c520c8e6f1619e05be170c19
Ben Anderson <andersjbe@gmail.com>
2020-03-22 22:57:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function johnLennonFacts(facts) {
   }
   return facts;
 }
+
+function iLoveTheBeatles(num) {
+  var results = [];
+  do {
+    results.push('I love the Beatles!')
+    num++;
+  } while (num < 15);
+  return results;
+}
