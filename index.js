1ddc86f3c0fd7b78f83f58d8b377c8b57ce1f934
Ben Anderson <andersjbe@gmail.com>
2020-03-22 22:53:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = []
+  for(let i = 0; i < musicians.length; i++) {
+    beatles.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return beatles;
+}
+
+function johnLennonFacts(facts) {
+  var index = 0;
+  while(index < facts.length) {
+    facts[index] += '!!!';
+    index++;
+  }
+  return facts;
+}
